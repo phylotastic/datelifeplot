fe0dadbfc4de2c687d5b9811f40b17b04d96e128
Luna Sare <sanchez.reyes.luna@gmail.com>
2022-02-07 14:09:18 -0800

    adding transparent bars as default to node age plots

--- R/plot_node_ages.R
+++ R/plot_node_ages.R
@@ -1,38 +1,45 @@
-# the node position on the y axis is given by pp$yy
+# @details The node position on the y axis is given by pp$yy
 # pp$yy also stores the positions of the tips
 # the y coordinate of nodes start at pp$yy[ape::Ntip + 1]
-#' Add Node Age/Calibration Data to a Plotted Phylogeny
-#' @param phy A phylo object with branch length proportional to time
+
+#' Plot a Chronogram and Add Node Age/Calibration Data to Nodes
+#' @description `plot_node_ages` plots a chronograms given in `phy` and adds age
+#'   data [points()] from `calibration_summary` to the corresponding nodes.
+#'
+#' @param phy A `phylo` object with branch length proportional to time
 #' @param cex_tips A numeric value indicating **c**haracter **ex**pansion (i.e.,
-#'  size scaling factor) of `phy` tip labels. Default to 1.
+#'  size scaling factor) for `phy` tip labels. Default to 1.
 #' @param calibration_summary An output of [datelife:::summary.matchedCalibrations()]
-#' @param color_points A named vector of colors. Names must correspond to study
-#'  in `calibration_summary$references`.
+#' @param color_points A named vector of colors. Names must correspond to study names
+#'  in `calibration_summary$in_phy$references`.
 #'  If vector is not named, colors will be recycled.
 #' @param pch A numeric vector indicating the type of point to plot.
 #'   See [graphics::par()] for options. Defaults to 20 = "bullet circle".
 #' @param cex_pch A numeric value indicating **c**haracter **ex**pansion (i.e.,
 #'  size scaling factor) of node age point symbols defined by `pch`. Default to 1.
 #' @param color_bars A character vector of one element indicating the color for
-#'  the bars. Default to "gray".
+#'  node age distribution bars. Defaults to "#80808050", which is hex for gray
+#' ("#808080") with 50\% transparency.
 #' @param lty_bars A numeric vector or character string indicating the plotting
-#'   line type for bars. See [graphics::par()] for options. Defaults to 1 = "solid".
+#'   line type for bars. See [graphics::par()] for options. Default to "solid" (1 if numeric).
 #' @param lwd_bars A numeric vector indicating the plotting width for bars.
-#'   See [graphics::par()] for options. Defaults to 2.
+#'   See [graphics::par()] for options. Default to 7.
 #' @importFrom ape .PlotPhyloEnv
 #' @export
 plot_node_ages <- function(phy,
                            cex_tips = 1,
                            calibration_summary,
                            color_points,
-                           pch = 20, # bullet circle
+                           pch = 20,
                            cex_pch = 1,
-                           color_bars,
-                           lty_bars,
-                           lwd_bars) {
-
+                           color_bars = "#80808050",
+                           lty_bars = "solid",
+                           lwd_bars = 7) {
+  # get calibrations that are in_phy only
+  in_phy <- calibration_summary$in_phy
   # obtain max x lim from ages
-  x_max <- max(c(calibration_summary$in_phy$MinAge, calibration_summary$in_phy$MaxAge))
+  x_max <- max(c(in_phy$MinAge,
+                 in_phy$MaxAge))
   # plot.phylo
   ape::plot.phylo(phy,
                   cex = cex_tips,
@@ -45,24 +52,26 @@ plot_node_ages <- function(phy,
   # get recorded plotting parameters
   lastPP <- get("last_plot.phylo", envir = .PlotPhyloEnv)
   # we use lastPP$xx positions to get plot x position for node ages
-  if (all(calibration_summary$in_phy$MinAge == calibration_summary$in_phy$MaxAge)) {
-    # We need the one max number in lastPP$xx, then we substract the node ages to get
-    # the actual x positions for all node ages on the plot:
-    x_ages <- max(lastPP$xx) - calibration_summary$in_phy$MinAge
+  ages_equal <- in_phy$MinAge == in_phy$MaxAge
+  if (all(ages_equal)) {
+    # We need the one max x position in lastPP$xx with max(lastPP$xx)
+    # Then we substract the node ages to get actual x positions for
+    # all node ages on the plot:
+    x_ages <- max(lastPP$xx) - in_phy$MinAge
   }
-  # we use lastPP$yy positions to get plotting y position of nodes
-  if (all(calibration_summary$in_phy$mrca_node_number > lastPP$Ntip)) {
+  # Next, we use lastPP$yy positions to get plotting y position of nodes
+  if (all(in_phy$mrca_node_number > lastPP$Ntip)) {
     # case in which mrca_node_numbers start at Ntip + 1
     # we can directly use mrca_node_numbers as index for lastPP$yy
     # it generates a vector of the correct length
-    y_nodes <- lastPP$yy[calibration_summary$in_phy$mrca_node_number]
+    y_nodes <- lastPP$yy[in_phy$mrca_node_number]
   }
-  # using references to color points
+  # Using references to color points
   if (missing(color_points)) {
-    color_points <- calibration_summary$in_phy$reference
+    color_points <- in_phy$reference
   }
-  for (i in unique(calibration_summary$in_phy$mrca_node_number)) {
-    rowsies <- calibration_summary$in_phy$mrca_node_number == i
+  for (i in unique(in_phy$mrca_node_number)) {
+    rowsies <- in_phy$mrca_node_number == i
     x_min <- min(x_ages[rowsies])
     x_max <- max(x_ages[rowsies])
     # we just need one element for y position of nodes:
@@ -71,9 +80,9 @@ plot_node_ages <- function(phy,
              y0 = y_pos,
              x1 = x_max,
              y1 = y_pos,
-             col = ifelse(missing(color_bars), "gray", color_bars),
-             lty = ifelse(missing(lty_bars), "solid", lty_bars),
-             lwd = ifelse(missing(lwd_bars), 2, lwd_bars))
+             col = color_bars,
+             lty = lty_bars,
+             lwd = lwd_bars)
   }
   graphics::points(x_ages,
          y_nodes,
